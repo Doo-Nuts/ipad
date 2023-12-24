e4dd0d5cbb2e736292e3dff099db9a4a94288e8b
Doo-Nuts <dkh9390@naver.com>
2023-12-24 13:08:41 +0900

    반응형 레이아웃 작업

--- js/main.js
+++ js/main.js
@@ -47,12 +47,15 @@ const searchInputEl = searchWrapEl.querySelector('input')
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 searchStarterEl.addEventListener('click', showSearch)
-searchCloserEl.addEventListener('click', hideSearch)
+searchCloserEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+  hideSearch()
+})
 searchShadowEl.addEventListener('click', hideSearch)
 
 function showSearch() {
   headerEl.classList.add('searching')
-  document.documentElement.classList.add('fixed') // documentElement : 최상위 요소 즉 html
+  stopscroll() // documentElement : 최상위 요소 즉 html
   // 검색 클릭시 메뉴 오른쪽부터 사라짐
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
@@ -66,7 +69,7 @@ function showSearch() {
 }
 function hideSearch() {
   headerEl.classList.remove('searching')
-  document.documentElement.classList.remove('fixed')
+  playscroll()
   // 검색 클릭시 메뉴 왼쪽부터 나타남
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
@@ -77,10 +80,75 @@ function hideSearch() {
   searchDelayEls.reverse()
   searchInputEl.value = '' // 검색 했던 텍스트를 닫으면 초기화
 }
+function playscroll() {
+  document.documentElement.classList.remove('fixed')
+}
+function stopscroll() {
+  document.documentElement.classList.add('fixed')
+}
 
+// 헤더 메뉴 토글!
+const menuStarterEl = document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click', function () {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing')
+    searchInputEl.value = '' // 모바일 검색창에 입력된 내용이 초기화
+    playscroll()
+  } else {
+    headerEl.classList.add('menuing')  
+    stopscroll()
+  }
+})
+
+// 헤더 검색!
+const searchTextFieldEl = document.querySelector('header .textfield')
+const searchCancelEl = document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click', function () {
+  headerEl.classList.add('searching--mobile')
+  searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click', function () {
+  headerEl.classList.remove('searching--mobile')
+  })
+
+
+//
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 740) {
+    headerEl.classList.remove('searching')
+  } else {
+    headerEl.classList.remove('searching--mobide')
+  }
+})
+
+
+//
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+
+navMenuToggleEl.addEventListener('click', function () {
+  if (navEl.classList.contains('menuing')) {
+    hideNavMenu()
+  } else {
+    showNavMenu()
+  }  
+  
+})
+
+navEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu)
+window.addEventListener('click', hideNavMenu)
+function showNavMenu() {
+  navEl.classList.add('menuing')
+}
+function hideNavMenu() {
+  navEl.classList.remove('menuing')
+}
 
 // 요소의 가시성 관찰
-
 const io = new IntersectionObserver(function (entries) {
   entries.forEach(function (entry) {
     if (!entry.isIntersecting) {
@@ -157,6 +225,7 @@ navigations.forEach(function (nav) {
   mapEl.innerHTML = /* html */ `
     <h3>
       <span class="text">${nav.title}</span>
+      <span class='icon'>+</span>
     </h3>
     <ul>
       ${mapList}
@@ -168,3 +237,11 @@ navigations.forEach(function (nav) {
 
 const thisyearEl = document.querySelector('span.this-year')
 thisyearEl.textContent = new Date().getFullYear()
+
+const mapEls = document.querySelectorAll('footer .navigations .map')
+mapEls.forEach(function (el) {
+  const h3El = el.querySelector('h3')
+  h3El.addEventListener('click', function () {
+    el.classList.toggle('active')
+  })
+})
